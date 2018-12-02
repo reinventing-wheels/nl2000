42f9fbac78132602d20fe6952d5e4832379f7832
foo <5144598+bar@users.noreply.github.com>
2018-12-02 20:17:57 +0300

    tests: a tougher test

--- src/convert.spec.ts
+++ src/convert.spec.ts
@@ -3,10 +3,12 @@ import { fast, slow } from './convert'
 describe('convert', () => {
   it('should convert an array of numbers from one radix to another', () => {
     for (const convert of [fast, slow]) {
-      const src = [0xdead, 0xf00d]
-      const dec = convert(0x10000, 100, [...src])
-      const hex = convert(100, 0x10000, [...dec])
-      expect(dec).toEqual([37, 35, 94, 11, 33])
+      const src = [...Array(0x100).keys()].reverse()
+      const dec = convert(0x100, 10, [...src])
+      const hex = convert(10, 0x100, [...dec])
+      expect(dec).toHaveLength(617)
+      expect(dec.slice(0, 10)).toEqual([3, 2, 3, 1, 6, 5, 0, 9, 0, 7])
+      expect(dec.slice(-10)).toEqual([3, 3, 3, 6, 1, 8, 3, 0, 4, 0])
       expect(hex).toEqual(src)
     }
   })
